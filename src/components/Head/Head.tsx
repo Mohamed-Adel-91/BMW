5ad0c31106818cecc3f40973f5cc0555ef3eb176
MohamedAdel <mohamed101291@gmail.com>
2023-11-03 15:23:07 +0200

    Add Carousel

--- src/components/Head/Head.tsx
+++ src/components/Head/Head.tsx
@@ -1,19 +1,44 @@
-import Carousel from 'react-bootstrap/Carousel';
-import "./Head.css"
-import head1 from '../../assets/img/head1.png';
+import Carousel from "react-bootstrap/Carousel";
+import "./Head.css";
+import headImg1 from "../../assets/img/headImg1.png";
+import headImg2 from "../../assets/img/headImg2.jpg";
+import headImg3 from "../../assets/img/headImg3.jpeg";
 
 function UncontrolledExample() {
-  return (
-    <Carousel>
-      <Carousel.Item>
-        <img src={head1} alt="head1" className='d-block w-100' style={{ height: "90vh" }} />
-        <Carousel.Caption>
-          <h3>BMW APPROVED USED CARS.</h3>
-          <p>Search, reserve and buy your Approved Used BMW - all online.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-  );
+    return (
+        <Carousel>
+            <Carousel.Item>
+                <img src={headImg1} alt="slide1" className="d-block w-100" />
+                <Carousel.Caption>
+                    <h3>BMW APPROVED USED CARS.</h3>
+                    <p>
+                        Search, reserve and buy your Approved Used BMW - all
+                        online.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img src={headImg2} alt="slide2" className="d-block w-100" />
+                <Carousel.Caption>
+                    <h3>BMW APPROVED USED CARS.</h3>
+                    <p>
+                        Search, reserve and buy your Approved Used BMW - all
+                        online.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img src={headImg3} alt="slide3" className="d-block w-100" />
+                <Carousel.Caption>
+                    <h3>BMW APPROVED USED CARS.</h3>
+                    <p>
+                        Search, reserve and buy your Approved Used BMW - all
+                        online.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    );
 }
 
 export default UncontrolledExample;
