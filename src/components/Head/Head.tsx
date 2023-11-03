99ccd9388e17477b810d9818a98523f1e145dd18
MohamedAdel <mohamed101291@gmail.com>
2023-11-03 16:57:19 +0200

    add css

--- src/components/Head/Head.tsx
+++ src/components/Head/Head.tsx
@@ -1,44 +1,26 @@
-import Carousel from "react-bootstrap/Carousel";
+// import Carousel from "react-bootstrap/Carousel";
 import "./Head.css";
 import headImg1 from "../../assets/img/headImg1.png";
-import headImg2 from "../../assets/img/headImg2.jpg";
-import headImg3 from "../../assets/img/headImg3.jpeg";
 
-function UncontrolledExample() {
+function Head() {
     return (
-        <Carousel>
-            <Carousel.Item>
-                <img src={headImg1} alt="slide1" className="d-block w-100" />
-                <Carousel.Caption>
-                    <h3>BMW APPROVED USED CARS.</h3>
-                    <p>
-                        Search, reserve and buy your Approved Used BMW - all
-                        online.
-                    </p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img src={headImg2} alt="slide2" className="d-block w-100" />
-                <Carousel.Caption>
-                    <h3>BMW APPROVED USED CARS.</h3>
-                    <p>
-                        Search, reserve and buy your Approved Used BMW - all
-                        online.
-                    </p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img src={headImg3} alt="slide3" className="d-block w-100" />
-                <Carousel.Caption>
-                    <h3>BMW APPROVED USED CARS.</h3>
-                    <p>
-                        Search, reserve and buy your Approved Used BMW - all
-                        online.
-                    </p>
-                </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
+        <div>
+            <div>
+                <img
+                    src={headImg1}
+                    alt="slide1"
+                    className="d-block w-100"
+                    style={{ height: "90vh" }}
+                />
+            </div>
+            <div className="title1">
+                <h3>BMW APPROVED USED CARS.</h3>
+                <p>
+                    Search, reserve and buy your Approved Used BMW - all online.
+                </p>
+            </div>
+        </div>
     );
 }
 
-export default UncontrolledExample;
+export default Head;
