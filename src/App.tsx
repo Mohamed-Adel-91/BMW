b565e66bc440650d321e814252e77e5e199c2e96
Mohamed Adel <mohamed101291@gmail.com>
2023-11-05 19:20:11 +0200

    finish header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-// import Elements from "./components/Elements/Elements"
-// import Head from "./components/Head/Head"
-import Header from "./components/Header"
-// import NavBar from "./components/NavBar/NavBar"
-// import Txt from "./components/Text/Txt"
+import Header from "./components/Header/Header"
+
 const App = () => {
   return (
     <div>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,12 +8,12 @@ const Header = () => {
     return (
         <div>
             <div className={header}>
-            <NavBar />
-            <div className='content'>
-                <h1 className='text-white py-home'>BMW APPROVED USED CARS.</h1>
-                <p className="py-home text-white">Search, reserve and buy your Approved Used BMW - all online.</p>
-            </div>
-            <Elements />
+                <NavBar />
+                <div className='content'>
+                    <h1 className='text-white py-home'>BMW APPROVED USED CARS.</h1>
+                    <p className="py-home text-white">Search, reserve and buy your Approved Used BMW - all online.</p>
+                </div>
+                <Elements />
             </div>
             <Filter />
         </div>
