2a4bb3eefedcd933512408797fa3031da5c727fb
Mohamed Adel <mohamed101291@gmail.com>
2023-11-08 18:46:34 +0200

    bottom main component

--- src/components/MainComponent/BottomMain/BottomMain.tsx
+++ src/components/MainComponent/BottomMain/BottomMain.tsx
@@ -1,7 +1,70 @@
+import { Button } from "react-bootstrap"
+import "./BottomMain.css"
 
 const BottomMain = () => {
     return (
-        <div>BottomMain</div>
+        <div className="BottomMain">
+            <div className="BottomMainCardOne">
+                <div className="BottomMainCardImg">
+                    <img src="/public/img/Group1.png" alt="" />
+                    <h2>WHY CHOOSE BMW FINANCIAL SERVICES?</h2>
+                </div>
+                <div className="BottomMainCardHover">
+                    <h1>BMW OWNER'S DIRECTORY.</h1>
+                    <p>As a proud owner, find everything you
+                        need for your BMW in one place,
+                        including BMW Accessories, 24/7
+                        Roadside Assistance, Warranties,
+                        Accident Support, Servicing and more.</p>
+                    <Button>Find Out More</Button>
+                </div>
+            </div>
+            <div className="BottomMainCardTwo">
+                <div className="BottomMainCardImg">
+                    <img src="/public/img/Path2.png" alt="" />
+                    <h2>FIND A BMW CENTRE.</h2>
+                </div>
+                <div className="BottomMainCardHover">
+                    <h1>BMW OWNER'S DIRECTORY.</h1>
+                    <p>As a proud owner, find everything you
+                        need for your BMW in one place,
+                        including BMW Accessories, 24/7
+                        Roadside Assistance, Warranties,
+                        Accident Support, Servicing and more.</p>
+                    <Button>Find Out More</Button>
+                </div>
+            </div>
+            <div className="BottomMainCardThree">
+                <div className="BottomMainCardImg">
+                    <img src="/public/img/Group3.png" alt="" />
+                    <h2>DOWNLOAD THE MY BMW APP.</h2>
+                </div>
+                <div className="BottomMainCardHover">
+                    <h1>BMW OWNER'S DIRECTORY.</h1>
+                    <p>As a proud owner, find everything you
+                        need for your BMW in one place,
+                        including BMW Accessories, 24/7
+                        Roadside Assistance, Warranties,
+                        Accident Support, Servicing and more.</p>
+                    <Button>Find Out More</Button>
+                </div>
+            </div>
+            <div className="BottomMainCardFour">
+                <div className="BottomMainCardImg">
+                    <img src="/public/img/Path2.png" alt="" />
+                    <h2>BMW OWNER'S DIRECTORY.</h2>
+                </div>
+                <div className="BottomMainCardHover">
+                    <h1>BMW OWNER'S DIRECTORY.</h1>
+                    <p>As a proud owner, find everything you
+                        need for your BMW in one place,
+                        including BMW Accessories, 24/7
+                        Roadside Assistance, Warranties,
+                        Accident Support, Servicing and more.</p>
+                    <Button>Find Out More</Button>
+                </div>
+            </div>
+        </div>
     )
 }
 
