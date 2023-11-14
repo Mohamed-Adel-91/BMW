9e3b720917446866a238b977432e6cc20afcbf80
Mohamed Adel <mohamed101291@gmail.com>
2023-11-14 18:04:57 +0200

    Center Home page

--- src/components/MainComponent/BottomMain/BottomMain.tsx
+++ src/components/MainComponent/BottomMain/BottomMain.tsx
@@ -1,10 +1,29 @@
 // import { Button } from "react-bootstrap"
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 import "./BottomMain.css"
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
 
 const BottomMain = () => {
     return (
-        <div className="BottomMain">
-            <div className="BottomMainCardOne">
+        <>
+            <Swiper
+                slidesPerView={4}
+                spaceBetween={30}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <div className="BottomMainCardOne">
                 <div className="BottomMainCardImg">
                     <img src="/public/img/Group1.png" alt="" />
                     <h2>WHY CHOOSE BMW FINANCIAL SERVICES?</h2>
@@ -19,12 +38,31 @@ const BottomMain = () => {
                     <Button>Find Out More</Button>
                 </div> */}
             </div>
-            <div className="BottomMainCardTwo">
-                <div className="BottomMainCardImg">
-                    <img src="/public/img/Path2.png" alt="" />
-                    <h2>FIND A BMW CENTRE.</h2>
-                </div>
-                {/* <div className="BottomMainCardHover">
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="BottomMainCardTwo">
+                        <div className="BottomMainCardImg">
+                            <img src="/public/img/Path2.png" alt="" />
+                            <h2>FIND A BMW CENTRE.</h2>
+                        </div>
+                        {/* <div className="BottomMainCardHover">
+                        <h1>BMW OWNER'S DIRECTORY.</h1>
+                        <p>As a proud owner, find everything you
+                        need for your BMW in one place,
+                        including BMW Accessories, 24/7
+                        Roadside Assistance, Warranties,
+                        Accident Support, Servicing and more.</p>
+                        <Button>Find Out More</Button>
+                        </div> */}
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="BottomMainCardThree">
+                        <div className="BottomMainCardImg">
+                            <img src="/public/img/Group3.png" alt="" />
+                            <h2>DOWNLOAD THE MY BMW APP.</h2>
+                        </div>
+                        {/* <div className="BottomMainCardHover">
                     <h1>BMW OWNER'S DIRECTORY.</h1>
                     <p>As a proud owner, find everything you
                         need for your BMW in one place,
@@ -32,40 +70,31 @@ const BottomMain = () => {
                         Roadside Assistance, Warranties,
                         Accident Support, Servicing and more.</p>
                     <Button>Find Out More</Button>
-                </div> */}
-            </div>
-            <div className="BottomMainCardThree">
-                <div className="BottomMainCardImg">
-                    <img src="/public/img/Group3.png" alt="" />
-                    <h2>DOWNLOAD THE MY BMW APP.</h2>
-                </div>
-                {/* <div className="BottomMainCardHover">
-                    <h1>BMW OWNER'S DIRECTORY.</h1>
-                    <p>As a proud owner, find everything you
+                    </div> */}
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="BottomMainCardFour">
+                        <div className="BottomMainCardImg">
+                            <img src="/public/img/Path2.png" alt="" />
+                            <h2>BMW OWNER'S DIRECTORY.</h2>
+                        </div>
+                        {/* <div className="BottomMainCardHover">
+                        <h1>BMW OWNER'S DIRECTORY.</h1>
+                        <p>As a proud owner, find everything you
                         need for your BMW in one place,
                         including BMW Accessories, 24/7
                         Roadside Assistance, Warranties,
                         Accident Support, Servicing and more.</p>
-                    <Button>Find Out More</Button>
-                </div> */}
-            </div>
-            <div className="BottomMainCardFour">
-                <div className="BottomMainCardImg">
-                    <img src="/public/img/Path2.png" alt="" />
-                    <h2>BMW OWNER'S DIRECTORY.</h2>
-                </div>
-                {/* <div className="BottomMainCardHover">
-                    <h1>BMW OWNER'S DIRECTORY.</h1>
-                    <p>As a proud owner, find everything you
-                        need for your BMW in one place,
-                        including BMW Accessories, 24/7
-                        Roadside Assistance, Warranties,
-                        Accident Support, Servicing and more.</p>
-                    <Button>Find Out More</Button>
-                </div> */}
-            </div>
-        </div>
-    )
+                        <Button>Find Out More</Button>
+                        </div> */}
+                    </div>
+                </SwiperSlide>
+            </Swiper >
+        </>
+    );
 }
+
+
 
 export default BottomMain
--- src/components/MainComponent/CenterMain/CenterMain.tsx
+++ src/components/MainComponent/CenterMain/CenterMain.tsx
@@ -4,7 +4,7 @@ const CenterMain = () => {
     return (
         <div className="CenterMainImgs">
             <div className="CenterMainFirstImg">
-                <h2>SELL BACK YOUR BMW.</h2>
+                <h1>SELL BACK YOUR BMW.</h1>
                 <p>
                     Why not sell your BMW back to the experts and get a
                     trusted price for your car? Simply provide us with the
@@ -14,7 +14,7 @@ const CenterMain = () => {
                 <a href="request">Request a Valuation.</a>
             </div>
             <div className="CenterMainSecondImg">
-                <h2>BENEFITS OF BUYING APPROVED USED.</h2>
+                <h1>BENEFITS OF BUYING APPROVED USED.</h1>
                 <ul>
                     <li>12+ mth BMW Warranty</li>
                     <li>12+ mth BMW Roadside Assistance</li>
@@ -24,8 +24,8 @@ const CenterMain = () => {
                 <a href="request">Request a Valuation.</a>
             </div>
             <div className="CenterMainThirdImg">
-                <h2>FINANCE YOUR CAR WITH
-                    BMW FINANCE OFFERS.</h2>
+                <h1>FINANCE YOUR CAR WITH
+                    BMW FINANCE OFFERS.</h1>
                 <p>
                     Configure your perfect finance deal with BMW Finance
                     Services on Approved Used Cars now. Search available
