bc26ff58b4914c1fdf5a38ed72028d6dfb3d8efb
Mohamed Adel <mohamed101291@gmail.com>
2023-11-19 19:08:59 +0200

    fix header && Nav bar

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,23 +1,130 @@
-import NavBar from './NavBar/NavBar'
-import styles from './Header.module.css'
-import Elements from './Elements/Elements'
-import Filter from './Filter/Filter'
+import React, { useState } from 'react';
+import NavBar from './NavBar/NavBar';
+import styles from './Header.module.css';
+import Elements from './Elements/Elements';
+import Filter from './Filter/Filter';
 
-const Header = () => {
-    const { header } = styles
+interface HeaderProps { }
+
+const Header: React.FC<HeaderProps> = () => {
+    const [backgroundImage, setBackgroundImage] = useState<string>(styles.top);
+    const [imageTitle, setImageTitle] = useState<string>('BMW APPROVED USED CARS.');
+    const [showAdditionalDiv, setShowAdditionalDiv] = useState<boolean>(true);
+
+    const handleModelLinkClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+        // Prevent the default behavior of the link click
+        event.preventDefault();
+        setBackgroundImage('anotherBackgroundImage');
+        setImageTitle('APPROVED USED BMW 3 SERIES.');
+        setShowAdditionalDiv(!showAdditionalDiv);
+    };
+
     return (
-        <div>
-            <div className={header}>
-                <NavBar />
-                <div className='content'>
-                    <h1 className='text-white py-home'>BMW APPROVED USED CARS.</h1>
-                    <p className="py-home text-white">Search, reserve and buy your Approved Used BMW - all online.</p>
+        <div className={styles.header}>
+            <div className={`${styles.top} ${styles[backgroundImage]}`}>
+                <nav className={styles.navBar}>
+                    <NavBar onModelLinkClick={handleModelLinkClick} />
+                </nav>
+                <div className={styles.content}>
+                    <h1 className='text-white py-home'>{imageTitle}</h1>
+                    {showAdditionalDiv && (<p className="py-home text-white">Search, reserve and buy your Approved Used BMW - all online.</p>)}
                 </div>
-                <Elements />
+                {showAdditionalDiv && (
+                    <div className={styles.elements}>
+                        <Elements />
+                    </div>
+                )}
             </div>
-            <Filter />
+            {showAdditionalDiv && (
+                <div className={styles.filter}>
+                    <Filter />
+                </div>)
+            }
         </div>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import NavBar from './NavBar/NavBar'
+// import styles from './Header.module.css'
+// import Elements from './Elements/Elements'
+// import Filter from './Filter/Filter'
+
+// const Header = () => {
+//     return (
+//         <div className={styles.header}>
+//             <div className={styles.top}>
+//                 <nav className={styles.navBar}>
+//                     <NavBar />
+//                 </nav>
+//                 <div className={styles.content}>
+//                     <h1 className='text-white py-home'>BMW APPROVED USED CARS.</h1>
+//                     <p className="py-home text-white">Search, reserve and buy your Approved Used BMW - all online.</p>
+//                 </div>
+//                 <div className={styles.elements}>
+//                     <Elements />
+//                 </div>
+//             </div>
+//             <div className={styles.filter}>
+//                 <Filter />
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default Header
--- src/Components/Header/NavBar/NavBar.tsx
+++ src/Components/Header/NavBar/NavBar.tsx
@@ -2,18 +2,26 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { AiOutlineSearch } from "react-icons/ai";
 import { CiLocationOn } from "react-icons/ci";
-import "./NavBar.css"
+import "./NavBar.css";
 
+interface NavBarProps {
+    onModelLinkClick: (event: React.MouseEvent<HTMLAnchorElement>) => void;
+}
 
-function NavBar() {
+function NavBar({ onModelLinkClick }: NavBarProps) {
     return (
         <>
             <div className='nav navHome'>
                 <div className="container">
                     <div className="row">
                         <div className="col navLinkHome">
-                            <Nav.Link className='linkHome' href="Models">Models</Nav.Link>
-                            <Nav.Link className='linkHome' href="Discover">Discover</Nav.Link>
+                            {/* Pass the onModelLinkClick function to the Models link */}
+                            <Nav.Link className='linkHome' onClick={onModelLinkClick} href="Models">
+                                Models
+                            </Nav.Link>
+                            <Nav.Link className='linkHome' href="Discover">
+                                Discover
+                            </Nav.Link>
                         </div>
                         <div className="col navIconsHome">
                             <Navbar.Brand className='icons' href="" style={{ fontSize: "30px" }}>
@@ -40,3 +48,49 @@ function NavBar() {
 }
 
 export default NavBar;
+
+
+
+
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import { AiOutlineSearch } from "react-icons/ai";
+// import { CiLocationOn } from "react-icons/ci";
+// import "./NavBar.css"
+
+
+// function NavBar() {
+//     return (
+//         <>
+//             <div className='nav navHome'>
+//                 <div className="container">
+//                     <div className="row">
+//                         <div className="col navLinkHome">
+//                             <Nav.Link className='linkHome' href="Models">Models</Nav.Link>
+//                             <Nav.Link className='linkHome' href="Discover">Discover</Nav.Link>
+//                         </div>
+//                         <div className="col navIconsHome">
+//                             <Navbar.Brand className='icons' href="" style={{ fontSize: "30px" }}>
+//                                 <AiOutlineSearch />
+//                             </Navbar.Brand>
+//                             <Navbar.Brand className='icons' href="" style={{ fontSize: "30px" }}>
+//                                 <CiLocationOn />
+//                             </Navbar.Brand>
+//                             <Navbar.Brand className='logo' href="Home">
+//                                 <img
+//                                     src="/public/img/BMW_White_Logo.svg.asset.1670245093434.svg"
+//                                     width="45"
+//                                     height="45"
+//                                     className="d-inline-block align-top"
+//                                     alt="React Bootstrap logo"
+//                                 />
+//                             </Navbar.Brand>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </>
+//     );
+// }
+
+// export default NavBar;
