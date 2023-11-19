9ea6331f28654276d92fd940a03016931a419f91
Mohamed Adel <mohamed101291@gmail.com>
2023-11-19 19:11:07 +0200

    fix header && Nav bar

--- src/Components/Header/NavBar/NavBar.tsx
+++ src/Components/Header/NavBar/NavBar.tsx
@@ -15,7 +15,6 @@ function NavBar({ onModelLinkClick }: NavBarProps) {
                 <div className="container">
                     <div className="row">
                         <div className="col navLinkHome">
-                            {/* Pass the onModelLinkClick function to the Models link */}
                             <Nav.Link className='linkHome' onClick={onModelLinkClick} href="Models">
                                 Models
                             </Nav.Link>
@@ -48,49 +47,3 @@ function NavBar({ onModelLinkClick }: NavBarProps) {
 }
 
 export default NavBar;
-
-
-
-
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-// import { AiOutlineSearch } from "react-icons/ai";
-// import { CiLocationOn } from "react-icons/ci";
-// import "./NavBar.css"
-
-
-// function NavBar() {
-//     return (
-//         <>
-//             <div className='nav navHome'>
-//                 <div className="container">
-//                     <div className="row">
-//                         <div className="col navLinkHome">
-//                             <Nav.Link className='linkHome' href="Models">Models</Nav.Link>
-//                             <Nav.Link className='linkHome' href="Discover">Discover</Nav.Link>
-//                         </div>
-//                         <div className="col navIconsHome">
-//                             <Navbar.Brand className='icons' href="" style={{ fontSize: "30px" }}>
-//                                 <AiOutlineSearch />
-//                             </Navbar.Brand>
-//                             <Navbar.Brand className='icons' href="" style={{ fontSize: "30px" }}>
-//                                 <CiLocationOn />
-//                             </Navbar.Brand>
-//                             <Navbar.Brand className='logo' href="Home">
-//                                 <img
-//                                     src="/public/img/BMW_White_Logo.svg.asset.1670245093434.svg"
-//                                     width="45"
-//                                     height="45"
-//                                     className="d-inline-block align-top"
-//                                     alt="React Bootstrap logo"
-//                                 />
-//                             </Navbar.Brand>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </>
-//     );
-// }
-
-// export default NavBar;
