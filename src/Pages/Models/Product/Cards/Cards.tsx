566471baad1a1a6d8a0e2cf0a4b0b22099851a3f
Mohamed Adel <mohamed101291@gmail.com>
2023-11-26 20:27:43 +0200

    add card elements

--- src/Pages/Models/Product/Cards/Cards.tsx
+++ src/Pages/Models/Product/Cards/Cards.tsx
@@ -3,33 +3,120 @@ import {
     Card,
     CardBody,
     CardSubtitle,
-    CardText,
     CardTitle,
 } from "reactstrap";
 import style from "./Cards.module.css";
 import "./Cards.module.css";
+import items from "./cards.json"
+import { MdOutlineCameraAlt } from "react-icons/md";
+import { CiLocationOn } from "react-icons/ci";
+import { IoMdStarOutline } from "react-icons/io";
+import { FaRegEye } from "react-icons/fa6";
+
+interface Items {
+    id: number,
+    carImg: string,
+    bmwLogo: string,
+    specification: string,
+    location: string,
+    favorite: string,
+    spec: string,
+    picNo: number,
+    name: string,
+    category: string,
+    fender: string,
+    kind: string,
+    model: string,
+    chassisNo: string,
+    km: string,
+    cashPrice: string,
+    perMonth: string,
+    deposit: string
+}
 
 const Cards = () => {
-    return (
-        <div className={style.Cards}>
+    const cardsElements = items.map((items: Items) => {
+        return (
             <Card
+                className={style.oneCard}
+                key={items.id}
                 style={{
-                    width: "18rem",
+                    width: "24rem",
                 }}
             >
-                <img alt="Sample" src="https://picsum.photos/300/200" />
+                <div className={style.headDiv}>
+                    <img className={style.carImg} style={{
+                        width: "100%",
+                        height: "300px",
+                    }} alt="Car Image" src={items.carImg} />
+                    <div className={style.specification}>
+                        {items.specification}
+                    </div>
+                    <div className={style.logoDiv}>
+                        <img className={style.logo} src={items.bmwLogo} alt="BMW LOGO" />
+                    </div>
+                    <div className={style.picNo}>
+                        <a href="#" id="smallIcon"><MdOutlineCameraAlt /></a>
+                        <span>{items.picNo}</span>
+                    </div>
+                    <div className={style.sideDiv}>
+                        <div className={style.location}>
+                            <span>{items.location}</span>
+                            <a href="#" id="smallIcon"><CiLocationOn /></a>
+                        </div>
+                        <div className={style.favorite}>
+                            <span>{items.favorite}</span>
+                            <a href="#" id="smallIcon"><IoMdStarOutline /></a>
+                        </div>
+                        <div className={style.spec}>
+                            <span>{items.spec}</span>
+                            <a href="#" id="smallIcon"><FaRegEye /></a>
+                        </div>
+                    </div>
+                </div>
                 <CardBody>
-                    <CardTitle tag="h5">Card title</CardTitle>
+                    <CardTitle tag="h5">{items.name}</CardTitle>
                     <CardSubtitle className="mb-2 text-muted" tag="h6">
-                        Card subtitle
+                        {items.category}
                     </CardSubtitle>
-                    <CardText>
-                        Some quick example text to build on the card title and
-                        make up the bulk of the card‘s content.
-                    </CardText>
-                    <Button>Button</Button>
+                    <div className={style.centerBar}>
+                        <div>{items.fender}</div>
+                        <div>{items.kind}</div>
+                        <div>{items.model}</div>
+                        <div>{items.chassisNo}</div>
+                        <div>{items.km}</div>
+                    </div>
+                    <div className={style.prices}>
+                        <div className={style.cashPrice}>
+                            <span>Cash Price</span>
+                            <span>{items.cashPrice}</span>
+                        </div>
+                        <div className={style.perMonth}>
+                            <span>Per Month</span>
+                            <span>{items.perMonth}</span>
+                        </div>
+                        <div className={style.deposit}>
+                            <span>Your Deposit</span>
+                            <span>{items.deposit}</span>
+                        </div>
+                    </div>
+                    <div className={style.cardFooter}>
+                        <div className={style.sideList}>
+                            <ul>
+                                <li><a href="#">Enquire Now</a></li>
+                                <li><a href="#">View Details</a></li>
+                            </ul>
+                        </div>
+                        <Button>Reserve & Buy</Button>
+                        <a href="#">Learn How To Reserve A Vehicle For E100</a>
+                    </div>
                 </CardBody>
             </Card>
+        )
+    })
+    return (
+        <div className={style.Cards}>
+            {cardsElements}
         </div>
     );
 };
