762270bdd34adc2aecd3e40064d2bda34bf16f79
MohamedAdel <mohamed101291@gmail.com>
2023-12-10 07:38:44 +0200

    fix responsive

--- src/Components/Header/Filter/Filter.tsx
+++ src/Components/Header/Filter/Filter.tsx
@@ -1,41 +1,39 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import './Filter.css'
-import { Row } from 'reactstrap';
+import "./Filter.css";
+import { Row, Col, Button } from "reactstrap";
+import { FormControl, FormSelect } from "react-bootstrap";
 
 function Filter() {
     return (
-        <Row className="con p-4">
-            <Form.Group className="row">
-                <Form.Group className="col">
-                    <Form.Control className="col" placeholder="Postcode/Location" />
-                </Form.Group>
-                <Form.Group className="col">
-                    <Form.Select className="col" placeholder="Nationwide">
-                        <option>Nationwide-1</option>
-                        <option>Nationwide-2</option>
-                        <option>Nationwide-3</option>
-                    </Form.Select>
-                </Form.Group>
-                <Form.Group className="col">
-                    <Form.Select className="col" placeholder="1 Series">
-                        <option>1 Series</option>
-                        <option>2 Series</option>
-                        <option>3 Series</option>
-                    </Form.Select>
-                </Form.Group>
-                <Form.Group className="col">
-                    <Form.Select className="col" placeholder="Select Model">
-                        <option>BMW 320i</option>
-                        <option>BMW X6</option>
-                        <option>BMW 520e</option>
-                    </Form.Select>
-                </Form.Group>
-                <Form.Group className="col">
-                    <Button type="submit" className="search">Search</Button>
-                </Form.Group>
-
-            </Form.Group>
+        <Row className="cont">
+            <Col>
+                <FormControl className="col" placeholder="Postcode/Location" />
+            </Col>
+            <Col>
+                <FormSelect className="col" placeholder="Nationwide">
+                    <option>Nationwide-1</option>
+                    <option>Nationwide-2</option>
+                    <option>Nationwide-3</option>
+                </FormSelect>
+            </Col>
+            <Col>
+                <FormSelect className="col" placeholder="Nationwide">
+                    <option>Nationwide-1</option>
+                    <option>Nationwide-2</option>
+                    <option>Nationwide-3</option>
+                </FormSelect>
+            </Col>
+            <Col>
+                <FormSelect className="col" placeholder="Select Model">
+                    <option>BMW 320i</option>
+                    <option>BMW X6</option>
+                    <option>BMW 520e</option>
+                </FormSelect>
+            </Col>
+            <Col>
+                <Button type="submit" className="search">
+                    Search
+                </Button>
+            </Col>
         </Row>
     );
 }
